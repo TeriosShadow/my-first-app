82547a73b297cbe5b55a7d01e0dfe0ff16126e43
TeriosShadow <TeriosShadow@gmail.com>
2019-04-27 17:33:10 -0500

    Updates

---
+++ precache-manifest.8ba3b4bab4c9fbc7b9917a48937b2ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdd202a0192b979fa76b95448da541f",
+    "url": "/my-first-app/index.html"
+  },
+  {
+    "revision": "3ce06eb97d173e5b6f33",
+    "url": "/my-first-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-first-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3ce06eb97d173e5b6f33",
+    "url": "/my-first-app/static/js/main.4b4d3be5.chunk.js"
+  },
+  {
+    "revision": "354d5acd385d2b7471af",
+    "url": "/my-first-app/static/js/runtime~main.73687222.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
