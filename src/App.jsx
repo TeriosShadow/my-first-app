70eded4f1b21d917bb4ce7eafe24ddfbc00a5614
TeriosShadow <TeriosShadow@gmail.com>
2019-04-27 18:53:31 -0500

    chore: change link text for testing purposes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ class App extends Component {
                     target="_blank"
                     rel="noopener noreferrer"
                     >
-                        Learn React
+                        Updating this link to some new text, still Learn React
                     </a>
                 </header>
             </div>
